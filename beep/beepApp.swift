063706888bf9adead4b00d133ce41056d59cb737
cher1shRXD <taewoo080401@gmail.com>
2025-03-29 09:23:18 +0900

    Initial Commit

---
+++ beep/beepApp.swift
@@ -0,0 +1,17 @@
+//
+//  beepApp.swift
+//  beep
+//
+//  Created by cher1shRXD on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct beepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
